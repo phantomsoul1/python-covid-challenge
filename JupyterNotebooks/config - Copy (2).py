05eca4e5f35a012b5ad5f5afd534feee52ea197c
Narender Sharma <ns30@hotmail.com>
2020-07-18 13:45:25 -0400

    naren July 18

---
+++ JupyterNotebooks/config - Copy (2).py
@@ -0,0 +1,5 @@
+#Add any placeholders for API keys here
+example_api_key = "Add API Keys Here"
+
+#CAUTION: DO NOT COMMIT/PUSH ANY VERSION OF THIS FILE THAT CONTAINS ACTUAL API KEYS! Instead, create a placeholder for a #particular API, like the one seen above (or like the ones provided with our homeworks) and commit/push that to the repository.
+#Otherwise, your API key may permanently be visible in the public (anyone can read) GitHub repository.
