1aa2fdb5e8c06773589db1e9436bfa5fd78c055d
Jake Byford <jake_byford@outlook.com>
2020-07-22 20:31:23 -0400

    Jake- Built platform for RapidAPI-COVID data

---
+++ JupyterNotebooks/Jake/config.py
@@ -0,0 +1,5 @@
+#Add any placeholders for API keys here
+api_key = "17aa415bbbmsh82a5765e5c65427p13b118jsnc974f2da4549"
+
+#CAUTION: DO NOT COMMIT/PUSH ANY VERSION OF THIS FILE THAT CONTAINS ACTUAL API KEYS! Instead, create a placeholder for a #particular API, like the one seen above (or like the ones provided with our homeworks) and commit/push that to the repository.
+#Otherwise, your API key may permanently be visible in the public (anyone can read) GitHub repository.
